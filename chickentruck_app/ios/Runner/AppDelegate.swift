86d80e2eb4ad5d959d2965026c8beb84cfe4ea40
Tobn211 <tobias-sauer@hotmail.de>
2025-10-03 12:19:47 +0200

    Update Flutter

--- chickentruck_app/ios/Runner/AppDelegate.swift
+++ chickentruck_app/ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
 import Flutter
 import UIKit
-
+import GoogleMaps   // ✅ hinzugefügt
+ 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCFljpaZh7AvrDTB5zkr2C0EkRm33mzkKE")  // ✅ API-Key setzen
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
